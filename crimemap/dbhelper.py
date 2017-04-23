14df3c784cc3bdb1f97534e2125c84b0467f2834
nirajkvinit <nirajkvinit@yahoo.co.in>
2017-04-23 19:49:25 +0530

    Chapter 6 commit

--- crimemap/dbhelper.py
+++ crimemap/dbhelper.py
@@ -22,9 +22,9 @@ class DBHelper:
     def add_input(self, data):
         connection = self.connect()
         try:
-            query = "insert into crimes (description) value ('{}');".format(data)
+            query = "insert into crimes (description) value (%s);"
             with connection.cursor() as cursor:
-                cursor.execute(query)
+                cursor.execute(query, data)
                 connection.commit()
         finally:
             connection.close()
