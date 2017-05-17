0666139b2913ae4753b195c572d8f3c9eda03987
nirajkvinit <nirajkvinit@yahoo.co.in>
2017-05-18 00:46:22 +0530

    Chapter 9 Completed

--- waitercaller/passwordhelper.py
+++ waitercaller/passwordhelper.py
@@ -5,10 +5,10 @@ import base64
 class PasswordHelper:
 
     def get_hash(self, plain):
-        return hashlib.sha512(plain).hexdigest()
+        return hashlib.sha512(plain.encode('utf-8')).hexdigest()
 
     def get_salt(self):
-        return base64.b64encode(us.urandom(20))
+        return str(base64.b64encode(os.urandom(20)))
 
     def validate_password(self, plain, salt, expected):
         return self.get_hash(plain + salt) == expected
