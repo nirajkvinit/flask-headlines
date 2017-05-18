3eafb6d40527bad090cd169197f20febc0e7847b
nirajkvinit <nirajkvinit@yahoo.co.in>
2017-05-19 00:55:43 +0530

    Chapter 10 incomplete

---
+++ waitercaller/bitlyhelper.py
@@ -0,0 +1,20 @@
+#import urllib2
+from urllib.request import urlopen
+import json
+
+TOKEN = "d170a7f79984f22a17e1cd98917c0dac52481997"
+ROOT_URL = "https://api-ssl.bitly.com"
+SHORTEN = "/v3/shorten?access_token={}&longUrl={}"
+
+class BitlyHelper:
+
+    def shorten_url(self, longurl):
+        try:
+            url = ROOT_URL + SHORTEN.format(TOKEN, longurl)
+            #response = urllib2.urlopen(url).read()
+            #response = urllib.request.urlopen(url).read()
+            response = urlopen(url).read().decode('utf8')
+            jr = json.loads(response)
+            return jr['data']['url']
+        except Exception as e:
+            print(e)
--- waitercaller/waitercaller.py
+++ waitercaller/waitercaller.py
@@ -1,3 +1,4 @@
+import datetime
 from flask import Flask
 from flask import render_template
 from flask import redirect
@@ -9,13 +10,17 @@ from flask_login import LoginManager
 from flask_login import login_required
 from flask_login import login_user
 from flask_login import logout_user
+from flask_login import current_user
 
+import config
 from mockdbhelper import MockDBHelper as DBHelper
 from user import User
 from passwordhelper import PasswordHelper
+from bitlyhelper import BitlyHelper
 
 DB = DBHelper()
 PH = PasswordHelper()
+BH = BitlyHelper()
 
 app = Flask(__name__)
 login_manager = LoginManager(app)
@@ -23,7 +28,7 @@ app.secret_key = 'GLTipXKTxg+E53YLzGa3M0tBqk1ZJmZjhGfimPQwOr+69wKOJ+hQeBJrqdfcZ2
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home_1.html")
 
 @app.route("/register", methods=["POST"])
 def register():
@@ -64,10 +69,50 @@ def load_user(user_id):
     if user_password:
         return User(user_id)
 
+@app.route("/dashboard")
+@login_required
+def dashboard():
+    now = datetime.datetime.now()
+    requests = DB.get_requests(current_user.get_id())
+    for req in requests:
+        deltaseconds = (now - req['time']).seconds
+        req['wait_minutes'] = "{}.{}".format((deltaseconds/60), str(deltaseconds % 60).zfill(2))
+    return render_template("dashboard.html", requests=requests)
+
 @app.route("/account")
 @login_required
 def account():
-    return "You are logged in"
+    tables = DB.get_tables(current_user.get_id())
+    return render_template("account.html", tables = tables)
+
+@app.route("/account/createtable", methods=["POST"])
+@login_required
+def account_createtable():
+    tablename = request.form.get("tablenumber")
+    tableid = DB.add_table(tablename, current_user.get_id())
+    #new_url = config.base_url + "newrequest/" + tableid
+    new_url = BH.shorten_url(config.base_url + "newrequest/" + tableid)
+    DB.update_table(tableid, new_url)
+    return redirect(url_for('account'))
+
+@app.route("/account/deletetable")
+@login_required
+def account_deletetable():
+    tableid = request.args.get("tableid")
+    DB.delete_table(tableid)
+    return redirect(url_for('account'))
+
+@app.route("/newrequest/<tid>")
+def newrequest(tid):
+    DB.add_request(tid, datetime.datetime.now())
+    return "Your request has been logged and a waiter will be with you shortly!"
+
+@app.route("/dashboard/resolve")
+@login_required
+def dashboard_resolve():
+    request_id = request.args.get("request_id")
+    DB.delete_request(request_id)
+    return redirect(url_for('dashboard'))
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
